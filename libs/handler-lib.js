1f8e2025f389fc1b011e5f9c1fe46607e8d1a598
Oliver Bigelow <oelucy@gmail.com>
2021-05-20 13:38:57 -0400

    added cors config

--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -7,12 +7,16 @@ export default function handler(lambda) {
             statusCode = 200;
         } catch (e) {
             body = { error: e.message };
-            statusCode= 500;
+            statusCode = 500;
         }
 
         return {
             statusCode,
-            body: JSON.stringify(body)
+            body: JSON.stringify(body),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            },
         };
     };
 }
